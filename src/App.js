0a720224bf8b5c530bc82ee382c5ce11480e2122
Camila Figueiredo <camila_fig@hotmail.com>
2024-08-13 19:28:11 -0300

    router

--- src/App.js
+++ src/App.js
@@ -1,57 +1,11 @@
 import express from "express"
-// import ProductManager from '../src/class/ProductManager.js'
 import productsRouter from './routes/products.router.js'
 
 const app = express()
-// const manager = new ProductManager("./data/products.json")
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 app.use('/products', productsRouter)
-
-// app.get("/products", async (req, res) => {
-//     const allProdcuts = await manager.conferProduct()
-//     try {
-//         res.status(200).json(allProdcuts)
-//     } catch (error) {
-//         res.status(500).json({ error: error.message })
-//     }
-// })
-
-// app.get("/products/limit", async (req, res) => {
-//     const { limit } = req.query
-//     const allProdcuts = await manager.conferProduct()
-//     const providedLimit = allProdcuts.slice(0, limit)
-//     return res.status(200).json(providedLimit)
-// })
-
-// app.get("/products/id/:pid", async (req, res) => {
-//     const { pid } = req.params
-//     const foundProductById = await manager.getProductById(Number(pid))
-//     try {
-//         if (!foundProductById) {
-//             res.status(404).json("Produto não existe.")
-//         } else {
-//             res.status(200).json(foundProductById)
-//         }
-//     } catch (error) {
-//         res.status(500).json({ error: error.message })
-//     }
-// })
-
-// app.get("/products/name/:title", async (req, res) => {
-//     const { title } = req.params
-//     const foundProductByName = await manager.getProductByName(title)
-//     try {
-//         if (!foundProductByName) {
-//             res.status(404).json("Produto não existe.")
-//         } else {
-//             res.status(200).json(foundProductByName)
-//         }
-//     } catch (error) {
-//         res.status(500).json({ error: error.message })
-//     }
-// })
 
 export default app
