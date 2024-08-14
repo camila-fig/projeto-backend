c88f5e7a751952f7250ed24b6e3b8730f6dc420d
Camila Figueiredo <camila_fig@hotmail.com>
2024-08-14 19:13:56 -0300

    controller

--- src/routes/products.router.js
+++ src/routes/products.router.js
@@ -1,67 +1,19 @@
 import express from "express"
-import ProductManager from '../../src/class/ProductManager.js'
 import validProduct from "../middleware/validProduct.js"
+import productController from "../controllers/productController.js"
 
 const router = express.Router()
-const manager = new ProductManager("./data/products.json")
 
-router.get("/", async (req, res) => {
-    const allProdcuts = await manager.conferProduct()
-    try {
-        res.status(200).json(allProdcuts)
-    } catch (error) {
-        res.status(500).json({ error: error.message })
-    }
-})
+router.get("/", productController.getAllProducts)
 
-router.get("/limit", async (req, res) => {
-    const { limit } = req.query
-    const allProdcuts = await manager.conferProduct()
-    const providedLimit = allProdcuts.slice(0, limit)
-    return res.status(200).json(providedLimit)
-})
+router.get("/limit", productController.getLimitProducts)
 
-router.get("/:pid", async (req, res) => {
-    const { pid } = req.params
-    const foundProductById = await manager.getProductById(Number(pid))
-    try {
-        if (!foundProductById) {
-            res.status(404).json("Produto não existe.")
-        } else {
-            res.status(200).json(foundProductById)
-        }
-    } catch (error) {
-        res.status(500).json({ error: error.message })
-    }
-})
+router.get("/:pid", productController.getById)
 
-router.post("/", validProduct, async (req, res) => {
-    try {
-        const product = req.body
-        const newProduct = await manager.addProduct(product)
-        return res.status(201).json(newProduct)
-    } catch (error) {
-        return res.status(500).json({ error: error.message })
-    }
-})
+router.post("/", validProduct, productController.createProduct)
 
-router.put("/:pid", async (req, res) => {
-    try {
-        const product = req.body
-        const { pid } = req.params
-        const updateProduct = await manager.updateProduct(product, Number(pid))
-        return res.status(200).json(updateProduct)
-    } catch (error) {
-        return res.status(500).json({ error: error.message })
-    }
-})
+router.put("/:pid", productController.updateProductById)
 
-router.delete("/:pid", async (req, res) => {
-    try {
-        const { pid } = req.params
-        await manager.deleteProductById(Number(pid))
-        return res.status(204).send()
-    } catch (error) { }
-})
+router.delete("/:pid", productController.deleteProduct)
 
 export default router
