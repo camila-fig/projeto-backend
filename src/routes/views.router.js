ac565305e1bb247deb4396eaef23bdc424696768
Camila Figueiredo <camila_fig@hotmail.com>
2024-09-26 21:37:59 -0300

    edit router

--- src/routes/views.router.js
+++ src/routes/views.router.js
@@ -1,21 +1,20 @@
 import express from "express"
+import validRole from "../middleware/validRole.js"
+import productsService from "../service/products.service.js"
 
 const router = express.Router()
 
-const validRole = (req, res, next) => {
-    if (req.session?.admin) {
-        return next()
-    } else {
-        res.render("msgForbidden")
-    }
-}
-
 router.get("/", (req, res) => { res.render("index") })
+
 router.get("/chat", (req, res) => { res.render("chat") })
-router.get("/admin", validRole, (req, res) => {
-    console.log("Admin:", req.session)
-    res.render("admin")
+
+router.get("/admin", validRole, async (req, res) => {
+    const result = await productsService.getProductsList()
+    const productsOrdered = result.sort((a, b) => a.code - b.code).sort((a, b) => a.title.localeCompare(b.title))
+    const products = productsOrdered.map((product) => product.toJSON())
+    res.render("admin", { products, productsOrdered })
 })
+
 router.get("/logout", (req, res) => {
     req.session.destroy(err => {
         if (!err) {
@@ -24,6 +23,14 @@ router.get("/logout", (req, res) => {
             res.send({ status: "Erro ao efetuar logout", body: err })
         }
     })
+})
+
+router.get("/edit/:pid", async (req, res) => {
+    const { pid } = req.params
+    let product = await productsService.getProductById(String(pid))
+    let resultProduct = [product]
+    resultProduct = resultProduct.map((product) => product.toJSON())
+    res.render("edit", { product: resultProduct[0] })
 })
 
 export default router
