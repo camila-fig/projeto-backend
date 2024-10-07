b0f06621a10cc0b474c1809d5a9186fe382be989
Camila Figueiredo <camila_fig@hotmail.com>
2024-10-07 14:04:59 -0300

    removed file system

--- src/routes/cart.router.js
+++ src/routes/cart.router.js
@@ -1,21 +1,33 @@
 import express from "express"
 import cartService from "../service/cart.service.js"
 import validCart from "../middleware/validCart.js"
-//import cartController from "../controllers/cartController.js"
 
 const router = express.Router()
 
-//Rotas com FileSystem
-//router.get("/", cartController.productsInCart)
-//router.post("/", cartController.createCart)
-//router.get("/:cid", cartController.getById)
-//router.post("/:cid/product/:pid", validCart, cartController.addToCart)
+router.get("/", async (req, res) => {
+    let cookieMail = req.cookies.EmailLogged
+    if (cookieMail == undefined) {
+        res.render("msgCart")
+    } else {
+        res.redirect('/:email')
+    }
 
-//Rotas com Mongoose
-router.get("/:email", validCart, async (req, res) => { 
-    const { email } = req.cookie
-    const cart = await cartService.addProductToCart(pid, cid)
-    
-    res.render("cart") })
+})
+
+router.get("/:email", async (req, res) => {
+    const { email } = req.cookies.EmailLogged
+
+
+    // if (cookieMail == undefined) {
+    //     const { email } = "withoutEmail"
+    //     res.render("msgCart")
+    // } else {
+    //     console.log("Cookie:", cookieMail)
+    //     const { email } = cookieMail
+    //     //const cart = await cartService.addProductToCart(pid, cid)
+
+    res.render("cart")
+    // }
+})
 
 export default router
