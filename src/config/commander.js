baba179d30bc6c8ccb941bae67eb2bb702d52f47
Camila Figueiredo <camila_fig@hotmail.com>
2024-10-21 14:54:10 -0300

    commander

--- src/config/commander.js
+++ src/config/commander.js
@@ -2,10 +2,10 @@ import { Command } from "commander"
 
 const program = new Command()
 
-program.option('-p', 'server port', 8080)
+program.option('-p <port>', 'server port', 8080)
 program.requiredOption('-u <user>', 'Usuário usando o aplicativo', 'Nenhum usuário declarado')
 program.parse()
 
-//console.log("Options:", program.opts())
+console.log("Options:", program.opts())
 
 export default program
